07ebaaf6d94b53047b26952f380c43cf9e337695
Andrei Tseniuta <gomelmf@gmail.com>
2025-03-19 09:54:20 +0100

    finish task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -212,8 +212,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => {
+    return fn(...args1, ...args2);
+  };
 }
 
 /**
@@ -233,8 +235,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let currentNumID = startFrom;
+  return () => {
+    const result = currentNumID;
+    currentNumID += 1;
+    return result;
+  };
 }
 
 module.exports = {
